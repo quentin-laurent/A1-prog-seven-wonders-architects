4616bcd4f5c73ebb742796bfac2951ddea800d73
quentinlaurent <quentin.laurent.sup@gmail.com>
2023-01-10 20:38:19 +0100

    updated Main

--- src/main/java/com/isep/game/Main.java
+++ src/main/java/com/isep/game/Main.java
@@ -1,5 +1,7 @@
 package com.isep.game;
 
+import com.isep.game.cards.Deck;
+import com.isep.game.wonders.Alexandria;
 import com.isep.utils.ConsoleOutput;
 import com.isep.utils.ConsoleParser;
 import jdk.jshell.spi.ExecutionControl;
@@ -9,6 +11,12 @@ public class Main
     public static void main(String[] args) throws ExecutionControl.NotImplementedException
     {
         Game game = new Game(new ConsoleParser(), new ConsoleOutput());
-        game.play();
+        //game.play();
+        Alexandria alexandria = new Alexandria();
+        Deck deck = alexandria.getDeck();
+        System.out.println(deck);
+        System.out.println();
+        deck.pickCard();
+        System.out.println(deck);
     }
 }
