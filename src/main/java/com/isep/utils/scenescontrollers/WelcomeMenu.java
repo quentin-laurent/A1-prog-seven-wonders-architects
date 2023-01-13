f40f1fa491d807ca53f3e7af433a8e2c412d8c11
Theophile-Wemaere <theophile.wemaere@gmail.com>
2023-01-14 00:16:47 +0100

    added event for button hovering on main menu

--- src/main/java/com/isep/utils/scenescontrollers/WelcomeMenu.java
+++ src/main/java/com/isep/utils/scenescontrollers/WelcomeMenu.java
@@ -15,45 +15,84 @@ import javafx.scene.text.Font;
 import java.io.IOException;
 import java.util.Objects;
 
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
 public class WelcomeMenu {
 
     @FXML
-    private Button sound, beginButton, leaveButton;
+    private Button soundButton, beginButton, leaveButton;
 
     private Clip clip = AudioSystem.getClip();
 
+    String button_style = "-fx-background-color: #506E3A;";
+    private float coeff_button = 10;
+    private int button_font_size = 26;
     public WelcomeMenu() throws LineUnavailableException {
     }
     public void initialize() throws IOException, UnsupportedAudioFileException, LineUnavailableException
     {
-        sound.setFont(Font.loadFont(GUIParser.class.getResourceAsStream("/fonts/MesloLGS-NF.ttf"), 51));
-        leaveButton.setFont(Font.loadFont(GUIParser.class.getResourceAsStream("/fonts/Helenium-bold.ttf"), 26));
-        beginButton.setFont(Font.loadFont(GUIParser.class.getResourceAsStream("/fonts/Helenium-bold.ttf"), 26));
+        beginButton.setOnMouseEntered(event -> expendButton(beginButton));
+        beginButton.setOnMouseExited(event -> reduceButton(beginButton));
+
+        leaveButton.setOnMouseEntered(event -> expendButton(leaveButton));
+        leaveButton.setOnMouseExited(event -> reduceButton(leaveButton));
+
+        soundButton.setFont(Font.loadFont(GUIParser.class.getResourceAsStream("/fonts/MesloLGS-NF.ttf"), 51));
+        leaveButton.setFont(Font.loadFont(GUIParser.class.getResourceAsStream("/fonts/Helenium-bold.ttf"), button_font_size));
+        beginButton.setFont(Font.loadFont(GUIParser.class.getResourceAsStream("/fonts/Helenium-bold.ttf"), button_font_size));
 
         this.clip.open(AudioSystem.getAudioInputStream(Objects.requireNonNull(GUIParser.class.getResourceAsStream("/musics/welcome.wav"))));
         if(StageLoader.sound)
             this.clip.loop(Clip.LOOP_CONTINUOUSLY);
         else
-            sound.setText("\uF026");
+            soundButton.setText("\uF026");
     }
+
+    private void expendButton(Button button)
+    {
+        button.setLayoutX(button.getLayoutX()-coeff_button/2);
+        button.setLayoutY(button.getLayoutY()-coeff_button/2);
+
+        button.setPrefWidth(button.getPrefWidth()+coeff_button);
+        button.setPrefHeight(button.getPrefHeight()+coeff_button);
+        button.setFont(Font.loadFont(GUIParser.class.getResourceAsStream("/fonts/Helenium-bold.ttf"), button_font_size+coeff_button/2.0));
+    }
+
+    private void reduceButton(Button button)
+    {
+
+        button.setLayoutX(button.getLayoutX()+coeff_button/2);
+        button.setLayoutY(button.getLayoutY()+coeff_button/2);
+
+        button.setPrefWidth(button.getPrefWidth()-coeff_button);
+        button.setPrefHeight(button.getPrefHeight()-coeff_button);
+        button.setFont(Font.loadFont(GUIParser.class.getResourceAsStream("/fonts/Helenium-bold.ttf"), button_font_size));
+    }
+
     @FXML
-    protected void onSoundButton(){
+    protected void onSoundButton()
+    {
         if(StageLoader.sound)
         {
             StageLoader.sound = false;
-            sound.setText("\uF026");
+            soundButton.setText("\uF026");
             this.clip.stop();
         }
         else
         {
             StageLoader.sound = true;
-            sound.setText("\uF028");
+            soundButton.setText("\uF028");
             this.clip.loop(Clip.LOOP_CONTINUOUSLY);
         }
     }
+
+    @FXML
+    protected void onBeginButton() throws IOException
+    {
+        StageLoader.loadFXMLScene("test");
+    }
+    @FXML
+    protected void onLeaveButton()
+    {
+        System.exit(0);
+    }
+
 }
