7203a8c92be0ab2e0920a6c15b2c0e5aae489b9e
quentinlaurent <quentin.laurent.sup@gmail.com>
2023-01-13 22:59:26 +0100

    added unit tests for Hand class

--- src/main/java/com/isep/game/cards/Hand.java
+++ src/main/java/com/isep/game/cards/Hand.java
@@ -23,7 +23,17 @@ public class Hand
         this.cards = new HashMap<Card, Integer>();
     }
 
+    // Getters & Setters
+    public HashMap<Card, Integer> getCards()
+    {
+        return this.cards;
+    }
+
     // Methods
+    public boolean isEmpty()
+    {
+        return this.cards.isEmpty();
+    }
 
     /**
      * Adds a new {@link Card} to this {@link Hand}.
@@ -32,7 +42,27 @@ public class Hand
      */
     public void addCard(Card card)
     {
-        this.cards.put(card, 1);
+        if(this.cards.containsKey(card))
+            this.cards.put(card, this.cards.get(card) + 1);
+        else
+            this.cards.put(card, 1);
+    }
+
+    /**
+     * Removes a {@link Card} from this {@link Hand}.
+     * @param card The {@link Card} to remove.
+     * @throws RuntimeException If the provided {@link Card} is not in this {@link Hand}.
+     * @author Quentin LAURENT
+     */
+    public void removeCard(Card card)
+    {
+        if(!(this.cards.containsKey(card)))
+            throw new RuntimeException(String.format("Failed to remove Card: [%s] from Hand: The Card provided is not in this Hand.", card));
+
+        if(this.cards.get(card) == 1)
+            this.cards.remove(card);
+        else
+            this.cards.put(card, this.cards.get(card) - 1);
     }
 
     /**
@@ -73,8 +103,8 @@ public class Hand
         StringBuilder s = new StringBuilder(String.format("%d cards:\n", this.cards.size()));
         for(var entry: this.cards.entrySet())
         {
-            s.append(entry.getKey().toString());
-            s.append("\n");
+            for(int i = 0; i < entry.getValue(); i++)
+                s.append(entry.getKey().toString()).append('\n');
         }
 
         // Removing the last \n char
---
+++ src/test/java/com/isep/game/cards/HandTest.java
@@ -0,0 +1,187 @@
+package com.isep.game.cards;
+
+import com.isep.game.wonders.Alexandria;
+import com.isep.game.wonders.Stage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandTest
+{
+    @Test
+    void newlyCreatedHandShouldBeEmpty()
+    {
+        Hand hand = new Hand();
+        assertTrue(hand.isEmpty());
+    }
+
+    @Test
+    void handWithCardsShouldNotBeEmpty()
+    {
+        Hand hand = new Hand();
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreenCard(GreenCard.ScienceSymbol.TABLET));
+
+        assertFalse(hand.isEmpty());
+    }
+
+    @Test
+    void handWithNoCardsLeftShouldBeEmpty()
+    {
+        Hand hand = new Hand();
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.removeCard(new GreyCard(GreyCard.Material.WOOD));
+
+        assertTrue(hand.isEmpty());
+    }
+
+    @Test
+    void addCardShouldAddANewCardToTheHand()
+    {
+        Hand hand = new Hand();
+        Card card = new GreyCard(GreyCard.Material.WOOD);
+        hand.addCard(card);
+
+        assertEquals(1, (int) hand.getCards().get(card));
+    }
+
+    @Test
+    void existingCardShouldStackIfAlreadyInTheHand()
+    {
+        Hand hand = new Hand();
+        Card card = new GreyCard(GreyCard.Material.WOOD);
+        hand.addCard(card);
+        hand.addCard(card);
+
+        assertEquals(2, (int) hand.getCards().get(card));
+    }
+
+    @Test
+    void removingCardAvailableInMultipleCopiesShouldDecreaseItsQuantity()
+    {
+        Hand hand = new Hand();
+        Card card = new GreyCard(GreyCard.Material.WOOD);
+        hand.addCard(card);
+        hand.addCard(card);
+        hand.removeCard(card);
+
+        assertEquals(1, hand.getCards().get(card));
+    }
+
+    @Test
+    void removeCardAvailableInOneCopyShouldRemoveItFromTheHand()
+    {
+        Hand hand = new Hand();
+        Card card = new GreyCard(GreyCard.Material.WOOD);
+        hand.addCard(card);
+        hand.removeCard(card);
+
+        assertNull(hand.getCards().get(card));
+    }
+
+    @Test
+    void removeCardNotAvailableInHandShouldThrowRuntimeException()
+    {
+        Hand hand = new Hand();
+        Card card = new GreyCard(GreyCard.Material.WOOD);
+
+        assertThrows(RuntimeException.class, () -> hand.removeCard(card));
+    }
+
+    @Test
+    void canBuildStageShouldReturnTrueWhenHandContainsCardsRequiredToBuildStage()
+    {
+        // Stage 1 of Alexandria requires 2 different resources
+        Stage stage1 = new Alexandria().getStages().get(0);
+        Hand hand = new Hand();
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreenCard(GreenCard.ScienceSymbol.TABLET));
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreyCard(GreyCard.Material.GLASS));
+
+        assertTrue(hand.canBuildStage(stage1));
+
+        // Stage 4 of Alexandria requires 3 identical resources
+        Stage stage4 = new Alexandria().getStages().get(3);
+        hand = new Hand();
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreenCard(GreenCard.ScienceSymbol.TABLET));
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreyCard(GreyCard.Material.GLASS));
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+
+        assertTrue(hand.canBuildStage(stage4));
+    }
+
+    @Test
+    void canBuildStageShouldReturnTrueWhenHandContainsCardsRequiredToBuildStageWithYellowCards()
+    {
+        // Stage 1 of Alexandria requires 2 different resources
+        Stage stage1 = new Alexandria().getStages().get(0);
+        Hand hand = new Hand();
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreenCard(GreenCard.ScienceSymbol.TABLET));
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new YellowCard());
+
+        assertTrue(hand.canBuildStage(stage1));
+
+        // Stage 4 of Alexandria requires 3 identical resources
+        Stage stage4 = new Alexandria().getStages().get(3);
+        hand = new Hand();
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreenCard(GreenCard.ScienceSymbol.TABLET));
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreyCard(GreyCard.Material.GLASS));
+        hand.addCard(new YellowCard());
+
+        assertTrue(hand.canBuildStage(stage4));
+    }
+
+    @Test
+    void canBuildStageShouldReturnFalseWhenHandDoesntContainsCardsRequiredToBuildStage()
+    {
+        // Stage 1 of Alexandria requires 2 different resources
+        Stage stage1 = new Alexandria().getStages().get(0);
+        Hand hand = new Hand();
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreenCard(GreenCard.ScienceSymbol.TABLET));
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+
+        assertFalse(hand.canBuildStage(stage1));
+
+        // Stage 4 of Alexandria requires 3 identical resources
+        Stage stage4 = new Alexandria().getStages().get(3);
+        hand = new Hand();
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreenCard(GreenCard.ScienceSymbol.TABLET));
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreyCard(GreyCard.Material.GLASS));
+        hand.addCard(new GreyCard(GreyCard.Material.GLASS));
+
+        assertFalse(hand.canBuildStage(stage4));
+    }
+
+    @Test
+    void testToString()
+    {
+        Hand hand = new Hand();
+        hand.addCard(new YellowCard());
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new GreyCard(GreyCard.Material.WOOD));
+        hand.addCard(new RedCard(0));
+        hand.addCard(new BlueCard(3, false));
+        hand.addCard(new GreenCard(GreenCard.ScienceSymbol.TABLET));
+
+        StringBuilder expectedString = new StringBuilder("5 cards:\n");
+        for(var entry: hand.getCards().entrySet())
+        {
+            for(int i = 0; i < entry.getValue(); i++)
+                expectedString.append(entry.getKey().toString()).append('\n');
+        }
+        int length = expectedString.length();
+        expectedString.replace(length - 1, length, "");
+
+        assertEquals(expectedString.toString(), hand.toString());
+    }
+}
